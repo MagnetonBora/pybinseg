b4e8dd1a05bfe836e69df6b79a02ee96c23341ab
Roman Dryndik <roman.dryndik.consultant@nielsen.com>
2020-01-29 10:03:24 +0100

    Project structure refactoring

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import json
-from tensorflow import keras
 import efficientnet.tfkeras
+from tensorflow import keras
 
 
-def load_model(config_path: str, weights_path: str) -> 'keras.Model':
+def load_model(config_path: str, weights_path: str) -> keras.Model:
     with open(config_path, 'r') as f:
         config = json.load(f)
         model = keras.models.model_from_config(config)
